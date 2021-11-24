20acb6161de7ea39ae1cb69a5ae2989c182feab2
eguzman_hasar <emmanuel.guzman@hasar.com>
2021-11-24 16:27:24 -0300

    subo chip perfil

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,15 +1,26 @@
+import Avatar from '@mui/material/Avatar';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import img_perfil from '../../assets/imagenes/perfil.jpg'
+
 const Header = () => {
 
     const arrayHeader = ['My cv'];
 
     return ( 
     <>
-        {arrayHeader.map((header) => (
-            <div className='div_link' key={header}>
-                <a href={'#' + header}>{header}</a>
-            </div>
-        ))}
-
+        <Stack direction="row" spacing={2}>
+            {arrayHeader.map((header) => (
+                <div className='div_link' key={header}>
+                    <a href={'#' + header}>{header}</a>
+                </div>
+            ))}
+            <Chip
+                avatar={<Avatar alt="Emmanuel" src={img_perfil} />}
+                label="Avatar"
+                variant="outlined"
+            />
+        </Stack>
         
     </> 
     );
